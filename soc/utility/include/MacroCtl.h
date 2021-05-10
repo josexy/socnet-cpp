01636ad8fa32a6c2f38479c9ace479d8152efcf4
joxrays <30660121+josexy@users.noreply.github.com>
2021-05-10 08:07:03 +0800

    macros

---
+++ soc/utility/include/MacroCtl.h
@@ -0,0 +1,11 @@
+#ifndef SOC_UTILITY_MACRO_CTL_H
+#define SOC_UTILITY_MACRO_CTL_H
+
+// #define SUPPORT_SSL_LIB
+// #define SUPPORT_MYSQL_LIB
+#define SUPPORT_LOCALPASSSTORE
+
+constexpr static const char *kPassFile = "./pass_store/user_password";
+constexpr static const char *kRealm = "socnet@test";
+
+#endif
