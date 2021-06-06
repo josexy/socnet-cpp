df0fbd176ddad4bc96843b35e10204440f885ad4
joxrays <30660121+josexy@users.noreply.github.com>
2021-06-06 09:53:38 +0800

    Add json config file

---
+++ soc/utility/include/AppConfig.h
@@ -0,0 +1,47 @@
+#ifndef SOC_UTILITY_APPCONFIG_H
+#define SOC_UTILITY_APPCONFIG_H
+
+#include "../../libjson/include/JsonFormatter.h"
+#include "../../libjson/include/JsonParser.h"
+#include <unordered_map>
+#include <variant>
+
+using namespace libjson;
+
+namespace soc {
+class AppConfig {
+public:
+  using Values = std::variant<std::string, int, bool, std::vector<std::string>>;
+  using ConfigMp =
+      std::unordered_map<std::string, std::unordered_map<std::string, Values>>;
+
+  static AppConfig &instance() {
+    static AppConfig config;
+    return config;
+  }
+
+  const Values &get(const std::string &key, const std::string &subKey);
+  bool exist(const std::string &key, const std::string &subKey);
+
+  ~AppConfig() {}
+
+private:
+  AppConfig(const std::string &config = "./config.json");
+
+  AppConfig(const AppConfig &) = delete;
+  AppConfig &operator==(const AppConfig &) = delete;
+  AppConfig(AppConfig &&) = delete;
+  AppConfig &operator==(AppConfig &&) = delete;
+
+private:
+  ConfigMp config_;
+};
+
+#define EXIST_CONFIG(KEY, SUBKEY) AppConfig::instance().exist((KEY), (SUBKEY))
+
+#define GET_CONFIG(T, KEY, SUBKEY)                                             \
+  std::get<T>(AppConfig::instance().get((KEY), (SUBKEY)))
+
+} // namespace soc
+
+#endif
---
+++ soc/utility/src/AppConfig.cc
@@ -0,0 +1,55 @@
+#include "../include/AppConfig.h"
+using namespace soc;
+
+AppConfig::AppConfig(const std::string &config) {
+  std::ifstream ifs(config, std::ios_base::in);
+  JsonParser parser(ifs);
+  ifs.close();
+  auto [objPtr, arrPtr, valuePtr] = parser.parse();
+
+  size_t n = objPtr->size();
+  for (size_t i = 0; i < n; i++) {
+    auto obj = objPtr->get(i);
+    auto value = obj.second->toJsonObject();
+    std::string key = obj.first->value<std::string>();
+    size_t m = value->size();
+    for (size_t j = 0; j < m; j++) {
+      auto subobj = value->get(j);
+      auto subvalue = subobj.second;
+      std::string subkey = subobj.first->value<std::string>();
+
+      switch (subvalue->type()) {
+      case JsonType::String:
+        config_[key][subkey] = subvalue->value<std::string>();
+        break;
+      case JsonType::Number:
+        config_[key][subkey] = subvalue->value<int>();
+        break;
+      case JsonType::Boolean:
+        config_[key][subkey] = subvalue->value<bool>();
+        break;
+      case JsonType::Array: {
+        auto arr = subvalue->toJsonArray();
+        std::vector<std::string> values;
+        for (size_t k = 0; k < arr->size(); k++) {
+          auto val = arr->get(k);
+          values.emplace_back(val->value<std::string>());
+        }
+        config_[key][subkey] = values;
+      } break;
+      default:
+        break;
+      }
+    }
+  }
+}
+
+const AppConfig::Values &AppConfig::get(const std::string &key,
+                                        const std::string &subKey) {
+  return config_.at(key).at(subKey);
+}
+
+bool AppConfig::exist(const std::string &key, const std::string &subKey) {
+  return config_.find(key) != config_.end() &&
+         config_[key].find(subKey) != config_[key].end();
+}
