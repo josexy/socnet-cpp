b87a484d35c50ea1fbe6bf9fa956373d36c453f5
joxrays <30660121+josexy@users.noreply.github.com>
2021-07-21 10:13:51 +0800

    update HttpAuth

--- soc/http/include/HttpAuth.h
+++ soc/http/include/HttpAuth.h
@@ -1,107 +1,106 @@
 #ifndef SOC_HTTP_HTTPAUTH_H
 #define SOC_HTTP_HTTPAUTH_H
 
-#include <string.h>
-
 #include "HttpPassStore.h"
+#include <string.h>
 
 namespace soc {
 namespace http {
 
-template <class HttpDerivedClass> class HttpAuth {
-public:
-  const HttpDerivedClass *get_pointer() const {
-    return static_cast<const HttpDerivedClass *>(this);
-  }
+enum class HttpAuthType { Basic = 0x01, Digest = 0x02 };
 
-  bool verify_user() const { return get_pointer()->verify_user(); }
+class HttpAuth {
+public:
+  virtual bool verify() const = 0;
+  virtual HttpAuthType type() const noexcept = 0;
 };
 
-template <class PassStore>
-class HttpBasicAuth : public HttpAuth<HttpBasicAuth<PassStore>> {
+class HttpBasicAuth : public HttpAuth {
 public:
-  friend class HttpRequest;
-
-  bool verify_user() const noexcept { return verify_successed_; }
+  explicit HttpBasicAuth(std::string_view user, std::string_view pass) {
+    user_ = std::string(user.data(), user.size());
+    pass_ = std::string(pass.data(), pass.size());
+    auto x = HttpPassStore::instance().fetch(user_);
+    if (!x.has_value())
+      return;
+    std::string hashv = user_ + ":" + kRealm + ":" + pass_;
+    verify_successed_ = EncodeUtil::md5HashEqual(hashv, x.value());
+  }
 
   const std::string &username() const noexcept { return user_; }
   const std::string &password() const noexcept { return pass_; }
+  bool verify() const override { return verify_successed_; }
+  HttpAuthType type() const noexcept override { return HttpAuthType::Basic; }
 
 private:
-  void verify_user_private(const std::string_view &user,
-                           const std::string_view &pass) {
-    user_ = std::string(user.data(), user.size());
-    pass_ = std::string(pass.data(), pass.size());
-    auto x = HttpPassStore<PassStore>::instance().fetch_password(user);
-    if (!x.first)
-      return;
-    std::string hashv =
-        std::string(user) + ":" + kRealm + ":" + std::string(pass);
-    verify_successed_ = EncodeUtil::md5_hash_equal(hashv, x.second);
-  }
-
   bool verify_successed_ = false;
   std::string user_;
   std::string pass_;
 };
 
-template <class PassStore>
-class HttpDigestAuth : public HttpAuth<HttpDigestAuth<PassStore>> {
+class HttpDigestAuth : public HttpAuth {
 public:
-  friend class HttpRequest;
-
-  bool verify_user() const noexcept { return verify_successed_; }
-
-  const std::string &digest_info() const noexcept { return digest_info_; }
-
-private:
-  void verify_user_private(
-      const std::string_view &user, const std::string_view &method_,
-      const std::string_view &uri, const std::string_view &nonce,
-      const std::string_view &nc, const std::string_view &cnonce,
-      const std::string_view &qop, const std::string_view &resp) {
+  explicit HttpDigestAuth(const std::string &digest_info,
+                          const HttpMap<std::string, std::string> &da)
+      : digest_info_(digest_info) {
     // h1 = md5(username:realm:password)
     // h2 = md5(method:uri)
     // resp = md5(h1:nonce:nc:cnonce:qop:h2)
 
-    // clear password
-    auto x = HttpPassStore<PassStore>::instance().fetch_password(user);
-    if (!x.first)
+    auto username = da.get("username");
+    if (!username.has_value())
       return;
+    // clear password
+    auto x = HttpPassStore::instance().fetch(username.value());
+    if (!x.has_value())
+      return;
+
+    auto method = da.get("method");
+    auto uri = da.get("uri");
+    auto nonce = da.get("nonce");
+    auto nc = da.get("nc");
+    auto cnonce = da.get("cnonce");
+    auto qop = da.get("qop");
+    auto response = da.get("response");
 
     char buffer[128]{0};
     // h1
-    std::string h1 = x.second;
+    std::string h1 = x.value();
 
     // h2
     ::memset(buffer, 0, 128);
-    ::sprintf(buffer, "%s:%s", method_.data(), uri.data());
-    std::string h2 = EncodeUtil::md5_hash(buffer);
+    ::sprintf(buffer, "%s:%s", method.value().data(), uri.value().data());
+    std::string h2 = EncodeUtil::md5Hash(buffer);
 
     // resp
     ::memset(buffer, 0, 128);
     ::strncat(buffer, h1.data(), h1.size());
-    if (nonce.size()) {
+    if (nonce.has_value()) {
       ::strncat(buffer, ":", 2);
-      ::strncat(buffer, nonce.data(), nonce.size());
+      ::strcat(buffer, nonce.value().data());
     }
-    if (nc.size()) {
+    if (nc.has_value()) {
       ::strncat(buffer, ":", 2);
-      ::strncat(buffer, nc.data(), nc.size());
+      ::strcat(buffer, nc.value().data());
     }
-    if (cnonce.size()) {
+    if (cnonce.has_value()) {
       ::strncat(buffer, ":", 2);
-      ::strncat(buffer, cnonce.data(), cnonce.size());
+      ::strcat(buffer, cnonce.value().data());
     }
-    if (qop.size()) {
+    if (qop.has_value()) {
       ::strncat(buffer, ":", 2);
-      ::strncat(buffer, qop.data(), qop.size());
+      ::strcat(buffer, qop.value().data());
     }
     ::strncat(buffer, ":", 2);
     ::strncat(buffer, h2.data(), h2.size());
 
-    verify_successed_ = EncodeUtil::md5_hash_equal(buffer, resp.data());
+    verify_successed_ =
+        EncodeUtil::md5HashEqual(buffer, response.value().data());
   }
+
+  const std::string &digestInfo() const noexcept { return digest_info_; }
+  bool verify() const override { return verify_successed_; }
+  HttpAuthType type() const noexcept override { return HttpAuthType::Digest; }
 
 private:
   bool verify_successed_ = false;
