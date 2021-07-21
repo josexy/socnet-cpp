ae6bbe542b91270ef82491cd74e439b0a78d5951
joxrays <30660121+josexy@users.noreply.github.com>
2021-07-21 10:17:18 +0800

    update & code refactoring

--- soc/utility/include/EncodeUtil.h
+++ soc/utility/include/EncodeUtil.h
@@ -1,9 +1,9 @@
 #ifndef SOC_UTILITY_ENCODEUTIL_H
 #define SOC_UTILITY_ENCODEUTIL_H
 
+#include "../../modules/base64/include/Base64.h"
 #include "../../net/include/Buffer.h"
 #include <functional>
-#include <string>
 
 namespace soc {
 namespace net {
@@ -12,37 +12,39 @@ class Buffer;
 
 struct EncodeUtil {
   // Gzip compress algorithm for http response body
-  static void gzipcompress(net::Buffer *tmp, net::Buffer *buf,
-                           const std::function<void(size_t)> &f,
-                           const std::function<void()> &g);
+  static void gzipCompress(net::Buffer *, net::Buffer *,
+                           const std::function<void(size_t)> &,
+                           const std::function<void()> &);
 
   // MD5
-  static unsigned char *md5_hash(void *, size_t, unsigned char[16]);
-  static std::string md5_hash(const std::string_view &);
-  static bool md5_hash_equal(const std::string_view &,
-                             const std::string_view &);
-  static std::string md5_hash_file(const char *filename);
+  static unsigned char *md5Hash(void *, size_t, unsigned char[16]);
+  static std::string md5Hash(std::string_view);
+  static bool md5HashEqual(std::string_view, std::string_view);
+  static std::string md5HashFile(const char *);
+
+  // MurmurHash2, by Austin Appleby
+  static unsigned int murmurHash2(std::string_view);
 
   // Base64 encode/decode
-  static std::string base64_encode(const std::string_view &);
-  static std::string base64_decode(const std::string_view &);
+  static std::string base64Encode(std::string_view);
+  static std::string base64Decode(std::string_view);
 
   // Url encode/decode
-  static unsigned char hex2dec(char c);
-  static char dec2hex(char c);
+  static unsigned char hex2dec(char);
+  static char dec2hex(char);
 
-  static std::string url_decode(const char *value, size_t size);
-  static std::string url_decode(const std::string_view &value);
-  static std::string url_decode(const char *value);
+  static std::string urlDecode(const char *, size_t);
+  static std::string urlDecode(std::string_view);
+  static std::string urlDecode(const char *);
 
-  static std::string url_encode(const char *value, size_t size);
-  static std::string url_encode(const std::string_view &value);
-  static std::string url_encode(const char *value);
+  static std::string urlEncode(const char *, size_t);
+  static std::string urlEncode(std::string_view);
+  static std::string urlEncode(const char *);
 
-  static void tolowers(char *s, size_t size);
-  static void touppers(char *s, size_t size);
+  static void toLowers(char *, size_t);
+  static void toUppers(char *, size_t);
 
-  static std::string generate_randrom_string(size_t);
+  static std::string genRandromStr(size_t);
 };
 } // namespace soc
 
